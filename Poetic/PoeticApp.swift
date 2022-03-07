93a15e6b1c160a2aa67dbdbabce74b1f28e3db7c
Dean Thompson <deanthompson.music@gmail.com>
2022-03-07 15:08:37 +0900

    Initial Commit

---
+++ Poetic/PoeticApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoeticApp.swift
+//  Poetic
+//
+//  Created by Dean Thompson on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct PoeticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
