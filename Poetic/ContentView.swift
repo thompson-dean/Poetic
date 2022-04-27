dc4ed0146a01fd1a0a686f197c0b8a874566fbc7
Dean Thompson <deanthompson.music@gmail.com>
2022-04-27 11:13:14 +0900

    Adding Tab bar pop to parent on double tap - still needs some work

--- Poetic/ContentView.swift
+++ Poetic/ContentView.swift
@@ -9,21 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     
+    
+    @State private var tabSelection = 1
+    @State private var tappedTwice: Bool = false
+    
+    @State private var home = UUID()
+    @State private var search = UUID()
+    @State private var favorites = UUID()
+    @State private var quotes = UUID()
+    @State private var menu = UUID()
+    
+    
     @StateObject var viewModel = SearchViewModel()
     @StateObject var pcViewModel = PersistenceController()
     
     
     var body: some View {
-        TabView {
+        var handler: Binding<Int> { Binding(
+            get: { self.tabSelection },
+            set: {
+                if $0 == self.tabSelection {
+                    tappedTwice = true
+                }
+                self.tabSelection = $0
+            }
+        )}
+        
+        
+        return TabView(selection: handler) {
             HomeView(viewModel: viewModel, pcViewModel: pcViewModel)
                 .tabItem {
                     Label("Home", systemImage: "house")
-                }
+                        .onChange(of: tappedTwice) { tappedTwice in
+                            guard tappedTwice else { return }
+                            home = UUID()
+                            self.tappedTwice = false
+                        }
+                }.tag(1)
                 
             SearchView(viewModel: viewModel, pcViewModel: pcViewModel)
                 .tabItem {
                     Label("Search", systemImage: "magnifyingglass")
-                }
+                        .onChange(of: tappedTwice) { tappedTwice in
+                            guard tappedTwice else { return }
+                            home = UUID()
+                            self.tappedTwice = false
+                        }
+                }.tag(2)
                
                 
             FavoritesView(viewModel: viewModel, pcViewModel: pcViewModel)
