2fee2eda8c784fbfbfeca6acacb9c0d70e9dbeca
Dean Thompson <deanthompson.music@gmail.com>
2022-09-03 09:58:09 +0900

    Added New views

---
+++ Poetic/View/PracticePoemView.swift
@@ -0,0 +1,20 @@
+//
+//  PracticePoemView.swift
+//  Poetic
+//
+//  Created by Dean Thompson on 2022/08/27.
+//
+
+import SwiftUI
+
+struct PracticePoemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PracticePoemView_Previews: PreviewProvider {
+    static var previews: some View {
+        PracticePoemView()
+    }
+}
