768de8d1384b8f42536cd27f653898f274c73bf0
Dean Thompson <deanthompson.music@gmail.com>
2022-09-04 22:09:17 +0900

    Home View Complete

---
+++ Poetic/View/PoemCard.swift
@@ -0,0 +1,20 @@
+//
+//  PoemCard.swift
+//  Poetic
+//
+//  Created by Dean Thompson on 2022/09/04.
+//
+
+import SwiftUI
+
+struct PoemCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PoemCard_Previews: PreviewProvider {
+    static var previews: some View {
+        PoemCard()
+    }
+}
