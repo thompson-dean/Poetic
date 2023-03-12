7209e71f7d43c10e60904a3bd40a25f9622ac7e0
Dean Thompson <deanthompson.music@gmail.com>
2023-03-12 17:43:59 +0900

    delete unused code

--- Poetic/View/Search/IntegratedSearchView.swift
+++ Poetic/View/Search/IntegratedSearchView.swift
@@ -38,42 +38,7 @@ struct IntegratedSearchView: View {
                     .pickerStyle(SegmentedPickerStyle())
                     .padding(.horizontal, 8)
                     VStack(alignment: .leading) {
-//                        HStack(spacing: 16) {
-//                            Button {
-//                                viewModel.isTitle = true
-//                            } label: {
-//                                Text("title")
-//                                    .fontWeight(viewModel.isTitle ? .bold : .regular)
-//                                    .fontWithLineHeight(font: .systemFont(ofSize: 16), lineHeight: 16)
-//                                    .padding(.horizontal, 8)
-//                                    .padding(.vertical, 8)
-//                                    .overlay(
-//                                        RoundedRectangle(cornerRadius: 8)
-//                                            .stroke(lineWidth: 2)
-//                                    )
-//                            }
-//                            .disabled(viewModel.isTitle)
-//                            .foregroundColor(viewModel.isTitle ? colorScheme == .light ? .lightThemeColor : .darkThemeColor : .primary)
-//                            Button {
-//                                viewModel.isTitle = false
-//                            } label: {
-//                                Text("author")
-//                                    .fontWeight(viewModel.isTitle ? .regular : .bold)
-//                                    .fontWithLineHeight(font: .systemFont(ofSize: 16), lineHeight: 16)
-//                                    .padding(.horizontal, 8)
-//                                    .padding(.vertical, 8)
-//                                    .overlay(
-//                                        RoundedRectangle(cornerRadius: 8)
-//                                            .stroke(lineWidth: 2)
-//                                    )
-//                            }
-//                            .disabled(!viewModel.isTitle)
-//                            .foregroundColor(viewModel.isTitle ? .primary : colorScheme == .light ? .lightThemeColor : .darkThemeColor)
-//                        }
-//                        .padding(.top, 4)
-//                        .padding(.horizontal, 10)
-
-
+                        
                         ScrollView(showsIndicators: false) {
                             if viewModel.isTitle {
                                 switch viewModel.searchState {
