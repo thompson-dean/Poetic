d0058a795787bbaa31f8d2300b839c4a7681a0af
Dean Thompson <deanthompson.music@gmail.com>
2023-07-17 21:06:27 +0900

    complete Core Data tests

--- PoeticTests/PersistenceControllerTests.swift
+++ PoeticTests/PersistenceControllerTests.swift
@@ -42,7 +42,7 @@ class PersistenceControllerTests: XCTestCase {
         return container
     }
     
-    func testAddFavoritePoem() {
+    func test_PersistenceController_addFavoritePoem_incrementsFavoritedPoems() {
         let id = UUID()
         let title = "Test Poem"
         let author = "Test Author"
@@ -60,13 +60,16 @@ class PersistenceControllerTests: XCTestCase {
         XCTAssertEqual(persistenceController.favoritedPoems.first?.linecount, linecount)
     }
     
-    func testFetchFavoritedPoems() {
+    func test_PersistenceController_fetchFavoritedPoems_returnsCorrectTitle() {
         persistenceController.addFavoritePoem(id: UUID(), title: "Test Title", author: "Test Author", lines: ["Test line 1"], linecount: "1")
         
         XCTAssertEqual(persistenceController.favoritedPoems.first?.title, "Test Title")
+        XCTAssertEqual(persistenceController.favoritedPoems.first?.author, "Test Author")
+        XCTAssertEqual(persistenceController.favoritedPoems.first?.lines, ["Test line 1"])
+        XCTAssertEqual(persistenceController.favoritedPoems.first?.linecount, "1")
     }
 
-    func testAddAndDeleteFavoritePoem() {
+    func test_PersistenceController_addAndDeleteFavoritePoem_returnsEmptyFavoritedPoems() {
         let id = UUID()
         
         persistenceController.addFavoritePoem(id: id, title: "Test Title", author: "Test Author", lines: ["Test line 1"], linecount: "1")
@@ -75,5 +78,58 @@ class PersistenceControllerTests: XCTestCase {
         guard let poem = persistenceController.favoritedPoems.first else { XCTFail("Failed to fetch poem"); return }
         persistenceController.deleteFavoritedPoemFromTappingStar(entity: poem)
         XCTAssertTrue(persistenceController.favoritedPoems.isEmpty)
+    }
+    
+    func test_PersistenceController_addQuote_incrementsQuotes() {
+        let id = UUID()
+        let title = "Test Quote"
+        let author = "Test Author"
+        let quote = "This is a test quote."
+        let lines = ["Line 1", "Line 2"]
+        let linecount = "2"
+
+        persistenceController.addQuote(id: id, title: title, author: author, quote: quote, lines: lines, linecount: linecount)
+
+        XCTAssertEqual(persistenceController.quotes.count, 1)
+        XCTAssertEqual(persistenceController.quotes.first?.id, id)
+        XCTAssertEqual(persistenceController.quotes.first?.title, title)
+        XCTAssertEqual(persistenceController.quotes.first?.author, author)
+        XCTAssertEqual(persistenceController.quotes.first?.quote, quote)
+    }
+
+    func test_PersistenceController_addAndDeleteQuote_returnsEmptyQuotes() {
+        let id = UUID()
+        let title = "Test Quote"
+        let author = "Test Author"
+        let quote = "This is a test quote."
+        let lines = ["Line 1", "Line 2"]
+        let linecount = "2"
+
+        persistenceController.addQuote(id: id, title: title, author: author, quote: quote, lines: lines, linecount: linecount)
+        XCTAssertEqual(persistenceController.quotes.first?.quote, quote)
+        
+        guard let quoteEntity = persistenceController.quotes.first else { XCTFail("Failed to fetch quote"); return }
+        persistenceController.removeQuoteFromQuotes(entity: quoteEntity)
+        XCTAssertTrue(persistenceController.quotes.isEmpty)
+    }
+
+    func test_PersistenceController_addAndDeleteViewedPoem_returnsEmptyViewedPoems() {
+        let id = UUID()
+        let title = "Test Poem"
+        let author = "Test Author"
+        let lines = ["Line 1", "Line 2"]
+
+        persistenceController.addViewedPoem(id: id, title: title, author: author, lines: lines)
+        XCTAssertEqual(persistenceController.viewedPoems.first?.title, title)
+        
+        guard let poem = persistenceController.viewedPoems.first else { XCTFail("Failed to fetch poem"); return }
+        persistenceController.deleteViewedPoem(entity: poem)
+        XCTAssertTrue(persistenceController.viewedPoems.isEmpty)
+    }
+
+    func test_PersistenceController_convertDateToString_returnsCorrectFormat() {
+        let testDate = Date(timeIntervalSince1970: 1626643200) // July 19, 2021
+        let dateAsString = persistenceController.convertDateToString(date: testDate)
+        XCTAssertEqual(dateAsString, "Jul 19, 2021")
     }
 }
