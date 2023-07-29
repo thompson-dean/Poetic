4554318500a591319178fb3f773bd4e448bc91c2
Dean Thompson <deanthompson.music@gmail.com>
2023-07-29 11:56:15 +0900

    test bitrise trigger

--- PoeticWatch/ContentView.swift
+++ PoeticWatch/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("TEST BITRISE")
+            Text("TEST BITRISE AGAIN")
         }
         .padding()
     }
