2a99b847737eda25d013c124bd99bd94be6b2448
Dean Thompson <deanthompson.music@gmail.com>
2024-06-07 19:20:09 +0900

    Fix TabView bug

--- Poetic/ContentView.swift
+++ Poetic/ContentView.swift
@@ -8,17 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    
-    @State private var tabSelection = 1
-    @State private var tappedTwice: Bool = false
-    
-    @State private var home = UUID()
-    @State private var search = UUID()
-    @State private var favorites = UUID()
-    @State private var quotes = UUID()
-    @State private var menu = UUID()
-    
     @StateObject var viewModel = PoemViewModel(apiService: APIService())
     @StateObject var pcViewModel = PersistenceController()
     @ObservedObject var storeKitManager: StoreKitManager
@@ -28,81 +17,27 @@ struct ContentView: View {
     var authors: Authors = Bundle.main.decode("Authors.json")
     
     var body: some View {
-        var handler: Binding<Int> { Binding(
-            get: { self.tabSelection },
-            set: {
-                if $0 == self.tabSelection {
-                    withAnimation {
-                        tappedTwice = true
-                    }
-                    
-                }
-                self.tabSelection = $0
-            }
-        )}
-        
-        return TabView(selection: handler) {
+        TabView {
             NewHomeView(viewModel: viewModel, pcViewModel: pcViewModel)
-                .id(home)
                 .tabItem {
                     Label("Home", systemImage: "house")
-                        .onChange(of: tappedTwice) { tappedTwice in
-                            guard tappedTwice else { return }
-                            home = UUID()
-                            self.tappedTwice = false
-                        }
                 }
-                .tag(1)
-                
             IntegratedSearchView(viewModel: viewModel, pcViewModel: pcViewModel)
-                .id(search)
                 .tabItem {
                     Label("Explore", systemImage: "magnifyingglass")
-                        .onChange(of: tappedTwice) { tappedTwice in
-                            guard tappedTwice else { return }
-                            search = UUID()
-                            self.tappedTwice = false
-                        }
                 }
-                .tag(2)
-               
-                
             FavoritesView(viewModel: viewModel, pcViewModel: pcViewModel)
-                .id(favorites)
                 .tabItem {
                     Label("Favorites", systemImage: "star")
-                        .onChange(of: tappedTwice) { tappedTwice in
-                            guard tappedTwice else { return }
-                            favorites = UUID()
-                            self.tappedTwice = false
-                        }
                 }
-                .tag(3)
-            
             QuoteView(viewModel: viewModel, pcViewModel: pcViewModel)
-                .id(quotes)
                 .tabItem {
                     Label("Quotes", systemImage: "quote.bubble.fill")
-                        .onChange(of: tappedTwice) { tappedTwice in
-                            guard tappedTwice else { return }
-                            quotes = UUID()
-                            self.tappedTwice = false
-                        }
                 }
-                .tag(4)
-            
             SettingsView(viewModel: viewModel, pcViewModel: pcViewModel, storeKitManager: storeKitManager)
-                .id(menu)
                 .tabItem {
-                    
                     Label("Menu", systemImage: "line.3.horizontal")
-                        .onChange(of: tappedTwice) { tappedTwice in
-                            guard tappedTwice else { return }
-                            menu = UUID()
-                            self.tappedTwice = false
-                        }
                 }
-                .tag(5)
         }
         .accentColor(.primary)
         .onAppear {
